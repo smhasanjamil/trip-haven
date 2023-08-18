004470ae2852aa1598679418e37f6056529509c0
smhasanjamil <smhasanjamil23@gmail.com>
2023-08-18 19:13:49 +0600

    Register page design

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,9 +1,48 @@
+import { Link } from 'react-router-dom';
 import './Register.css';
 
 const Register = () => {
     return (
         <div>
-            <h3>This is register</h3>
+
+
+            <div className="hero min-h-screen ">
+                <div className="hero-content flex-col">
+                    <div className="text-center lg:text-left">
+                        <h1 className="text-5xl font-bold">Register</h1>
+                    </div>
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <div className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" placeholder="name" name='name' className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" placeholder="email" name='email' className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" placeholder="password" name='password' className="input input-bordered" />
+                                <label className="label">
+                                    <span className="label-text-alt  ">Already Have an account? <Link to='/login' className='link link-hover'>Login</Link></span>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-neutral">Register</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
         </div>
     );
 };
