0d657b64dc0fe73be4b0bcaf48b8e1713e0dff7a
smhasanjamil <smhasanjamil23@gmail.com>
2023-08-18 14:44:09 +0600

    Project setup with react, tailwind, daisyui

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div>
+            This is footer
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div>
+            This is home
+        </div>
+    );
+};
+
+export default Home;
