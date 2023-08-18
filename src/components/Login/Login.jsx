3617db3e8127c86161ab3c22f2af89e516377ef7
smhasanjamil <smhasanjamil23@gmail.com>
2023-08-18 19:16:06 +0600

    Login page design

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,40 @@
+import { Link } from 'react-router-dom';
 import './Login.css';
 
 const Login = () => {
     return (
         <div>
-            <h3>This is login</h3>
+
+            <div className="hero min-h-screen ">
+                <div className="hero-content flex-col">
+                    <div className="text-center lg:text-left">
+                        <h1 className="text-5xl font-bold">Login</h1>
+                    </div>
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <div className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" placeholder="email" name='email' className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" placeholder="password" name='password' className="input input-bordered" />
+                                <label className="label">
+                                    <span className="label-text-alt  ">Dont have an account? <Link to='/register' className='link link-hover'>Register</Link></span>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-neutral">Login</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
